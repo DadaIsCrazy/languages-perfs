4cf52206f3208cca60282a0926192fb7f8effb1d
DadaIsCrazy <darius.mercadier@gmail.com>
2019-11-20 15:28:25 +0100

    Improve style in Ruby implem (use .times rather than .each)

--- src/primes.rb
+++ src/primes.rb
@@ -6,7 +6,7 @@ nums = Array.new(max,false)
 nums[0] = nums[1] = true
 
 # Computing prime numbers
-(0..Math.sqrt(max)).each do |i|
+(Math.sqrt(max)).ceil.times do |i|
   next if nums[i]
   (i*2 .. max).step(i).each do |j|
     nums[j] = true
