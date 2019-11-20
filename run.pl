9ff56614b3f41330fc1e56158e6572b8226ae15a
DadaIsCrazy <darius.mercadier@gmail.com>
2019-11-20 23:08:15 +0100

    Separate run time and compile time

--- run.pl
+++ run.pl
@@ -17,8 +17,9 @@ make_path 'bin' unless -d 'bin';
 
 my $sbcl_space_size = 16384;
 my $node_space_size = 16384;
-my $repeat = 10;
+my $repeat = 2;
 my $long = 100; # if execution time > $long, then don't run it more than once
+my @bounds = (1000, 10000);#, 100000, 1000000, 10000000, 100000000, 1000000000);
 
 my %runners = (
     'Perl'                =>   \&run_perl,
@@ -26,48 +27,120 @@ my %runners = (
     'Go'                  =>   \&run_go,
     'Java'                =>   \&run_java,
     'Javascript (node)'   =>   \&run_javascript,
-    'Common Lisp (sbcl)'  =>   \&run_sbcl,
-    'OCaml (native)'      =>   \&run_ocaml_native,
-    'OCaml (bytecode)'    =>   \&run_ocaml_bytecode,
-    'PHP'                 =>   \&run_php,
-    'Python (CPython)'    =>   \&run_cpython,
-    'Python (Pypy)'       =>   \&run_pypy,
+    # 'Common Lisp (sbcl)'  =>   \&run_sbcl,
+    # 'OCaml (native)'      =>   \&run_ocaml_native,
+    # 'OCaml (bytecode)'    =>   \&run_ocaml_bytecode,
+    # 'PHP'                 =>   \&run_php,
+    # 'Python (CPython)'    =>   \&run_cpython,
+    # 'Python (Pypy)'       =>   \&run_pypy,
     'Python (numpy)'      =>   \&run_numpy,
-    'Racket'              =>   \&run_racket,
-    'Bash'                =>   \&run_bash,
+    # 'Racket'              =>   \&run_racket,
+    # 'Bash'                =>   \&run_bash,
     'R'                   =>   \&run_R
     );
 
-pre_run();
+my %compile_hooks = (
+    'C (gcc)'             => \&compile_C,
+    'Go'                  => \&compile_go,
+    'Java'                => \&compile_java,
+    'Common Lisp (sbcl)'  => \&compile_sbcl,
+    'OCaml (native)'      => \&compile_ocaml_native,
+    'OCaml (bytecode)'    => \&compile_ocaml_bytecode
+    );
 
-my %res;
-for my $n (1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000) {
-    my $correct_res = run_C($n);
+
+pre_run();
+my $compile_times = bench_compile();
+my $run_times = bench_runs();
+print_results_full($run_times, $compile_times);
+print_results_compact($run_times, $compile_times);
+
+
+
+sub bench_compile {
+    my %compile_times;
     for (1 .. $repeat) {
-        for my $lang (keys %runners) {
-            if ($n > 1000000 && $lang eq 'Bash') {
-                $res{$lang}->{$n} = [ '-' ];
-                next;
-            }
-            next if too_slow($lang, $n);
-            printf "\033[2K\rRunning [ %-19s]; n = %d", $lang, $n;
-            my ($res, $time) = time_fun($runners{$lang}, $n);
-            if (looks_like_number($res) && $res == $correct_res) {
-                push @{$res{$lang}->{$n}}, $time;
-            } else {
-                push @{$res{$lang}->{$n}}, "-";
+        for my $lang (keys %compile_hooks) {
+            printf "\033[2K\rCompiling [ %-19s] ($_/$repeat)", $lang;
+            my (undef, $time) = time_fun($compile_hooks{$lang});
+            push @{$compile_times{$lang}}, $time;
+        }
+    }
+    print "\033[2K\r";
+    return \%compile_times;
+}
+
+sub bench_runs {
+    my %run_times;
+    for my $n (@bounds) {
+        my $correct_res = run_C($n);
+        for (1 .. $repeat) {
+            for my $lang (keys %runners) {
+                if ($n > 1000000 && $lang eq 'Bash') {
+                    # Bash would be way too slow for n > 1000000
+                    $run_times{$lang}->{$n} = [ '-' ];
+                    next;
+                }
+                next if too_slow(\%run_times, $lang, $n);
+                printf "\033[2K\rRunning [ %-19s]; n = %d", $lang, $n;
+                my ($res, $time) = time_fun($runners{$lang}, $n);
+                if (looks_like_number($res) && $res == $correct_res) {
+                    push @{$run_times{$lang}->{$n}}, $time;
+                } else {
+                    push @{$run_times{$lang}->{$n}}, "-";
+                }
             }
         }
     }
+    print "\033[2K\r";
+    return \%run_times;
 }
-print "\033[2K\r";
 
+sub print_results_full {
+    my ($run_times, $compile_times) = @_;
+    open my $FH, '>', 'raw_data.txt' or die $!;
+    print $FH "| **Language**        | Compilation |",
+      (join " | ", map { sprintf " %-13s ", $_ } @bounds), " |\n";
+    print $FH "| ------------------- | ----------- |",
+      (join " | ", map { " " . ("-"x13) . " " } @bounds), " |\n";
+    for my $lang (sort {sort_langs($run_times)} keys %$run_times) {
+        my $format_str = "| %-19s | %-11s |" .
+            (join " | ", map { " %-13s " }
+                 keys %{$run_times->{$lang}}) . " |\n";
+        printf $FH $format_str, $lang,
+        (exists $compile_times->{$lang} ? mean($compile_times->{$lang}) : "-"),
+          map { mean($run_times->{$lang}{$_}) }
+            sort { $a <=> $b } keys %{$run_times->{$lang}};
+    }
+}
 
-for my $lang (keys %res) {
-    my $format_str = "%-20s | " . (join " | ", map { " %13s " } keys %{$res{$lang}}) . "\n";
-    printf $format_str, $lang, 
-      map { mean($res{$lang}{$_}) }
-        sort { $a <=> $b } keys %{$res{$lang}};
+sub print_results_compact {
+    my ($run_times, $compile_times) = @_;
+    print "| **Language**        | Compilation |",
+      (join "|", map { sprintf " %-*s ", length, $_ } @bounds), "|\n";
+    print "| ------------------- | ----------- |",
+      (join "|", map { sprintf " %-*s ", length, ($_=~s/./-/gr) } @bounds), "|\n";
+    for my $lang (sort {sort_langs($run_times)} keys %$run_times) {
+        my $format_str = "| %-19s | %-11s |" .
+            (join "|", map { " %-" . (length $_) . "s " } @bounds) . "|\n";
+        printf $format_str, $lang,
+        (exists $compile_times->{$lang} ?
+           mean($compile_times->{$lang}) =~ s/(\S+) *\S+/$1/r : "-"),
+          map { my $r = mean($run_times->{$lang}{$_}) =~ s/(\S+) *\S+/$1/r;
+                $_ == 1000000000 ? "**" . ($r =~ s/\..*//r) . "**" : $r }
+            sort { $a <=> $b } keys %{$run_times->{$lang}};
+    }
+}
+
+sub sort_langs {
+    my ($run_times) = @_;
+    my $rta = $run_times->{$a}->{$bounds[-1]}->[0];
+    my $rtb = $run_times->{$b}->{$bounds[-1]}->[0];
+    $rta = 0 if $rta !~ /\d/;
+    $rtb = 0 if $rtb !~ /\d/;
+    $rtb = -1 if $a =~ /numpy/;
+    $rta = -1 if $b =~ /numpy/;
+    return $rta <=> $rtb;
 }
 
 # Runs |$fun| with arguments |@args|, and returns the results as well
@@ -102,14 +175,14 @@ sub mean {
 # execution of $lang with $n didn't produce the valid result (because
 # in that case, no need to rerun it)
 sub too_slow {
-    my ($lang, $n) = @_;
-    return 0 unless $res{$lang}->{$n}; # First execution
-    return 1 unless looks_like_number($res{$lang}->{$n}->[-1]);
-    return $res{$lang}->{$n}->[-1] > $long;
+    my ($run_times, $lang, $n) = @_;
+    return 0 unless $run_times->{$lang}->{$n}; # First execution
+    return 1 unless looks_like_number($run_times->{$lang}->{$n}->[-1]);
+    return $run_times->{$lang}->{$n}->[-1] > $long;
 }
 
 # Sub to be run before running the other ones, doing stuffs that
-# shouldn't be timed.
+# shouldn't be timed neither in the run time nor in the compile time.
 sub pre_run {
     # Due to how OCaml is compiled; it's easier to put the source in
     # the bin folder.
@@ -122,22 +195,29 @@ sub run_perl {
     return $res;
 }
 
+sub compile_C {
+    system "gcc -O3 -o bin/primes_c src/primes.c -lm";
+}
 sub run_C {
     my ($n) = @_;
-    system "gcc -O3 -o bin/primes_c src/primes.c -lm";
     chomp(my $res = `./bin/primes_c $n 2>/dev/null`);
     return $res;
 }
 
+sub compile_go {
+    system "go build -o bin/primes_go src/primes.go"
+}
 sub run_go {
     my ($n) = @_;
-    chomp(my $res = `go run src/primes.go $n 2>/dev/null`);
+    chomp(my $res = `./bin/primes_go $n 2>/dev/null`);
     return $res;
 }
 
+sub compile_java {
+    system "javac -d bin src/primes.java";
+}
 sub run_java {
     my ($n) = @_;
-    system "javac -d bin src/primes.java";
     chomp(my $res = `java -cp bin Primes $n 2>/dev/null`);
     return $res;
 }
@@ -148,22 +228,29 @@ sub run_javascript {
     return $res;
 }
 
+sub compile_sbcl {
+    system q{sbcl --noinform --eval '(compile-file "src/primes.lisp" :output-file #P"../bin/primes")' --eval "(quit)" > /dev/null 2>&1};
+}
 sub run_sbcl {
     my ($n) = @_;
-    chomp(my $res = `sbcl --dynamic-space-size $sbcl_space_size --script src/primes.lisp $n 2>/dev/null`);
+    chomp(my $res = `sbcl --dynamic-space-size $sbcl_space_size --noinform --load "bin/primes.fasl" --quit --end-toplevel-options $n 2>/dev/null`);
     return $res;
 }
 
+sub compile_ocaml_native {
+    system "ocamlopt bin/primes.ml -o bin/primes_ml_native";
+}
 sub run_ocaml_native {
     my ($n) = @_;
-    system "ocamlopt bin/primes.ml -o bin/primes_ml_native";
     chomp(my $res = `./bin/primes_ml_native $n 2>/dev/null`);
     return $res;
 }
 
+sub compile_ocaml_bytecode {
+    system "ocamlc bin/primes.ml -o bin/primes_ml_bytecode";
+}
 sub run_ocaml_bytecode {
     my ($n) = @_;
-    system "ocamlc bin/primes.ml -o bin/primes_ml_bytecode";
     chomp(my $res = `./bin/primes_ml_bytecode $n 2>/dev/null`);
     return $res;
 }
