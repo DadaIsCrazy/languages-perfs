c8e5e2ad04d2568fe704e20af100e231330fa82d
DadaIsCrazy <darius.mercadier@gmail.com>
2019-11-20 10:34:13 +0100

    Make lisp implementation more lispian

--- src/primes.lisp
+++ src/primes.lisp
@@ -1,13 +1,5 @@
 (defvar mmax (parse-integer (cadr *posix-argv*)))
 
-(defun for-step (f i j s)
-  (if (>= i j)
-      ()
-    (progn
-      (funcall f i)
-      (for-step f (+ i s) j s))))
-
-
 (defvar nums (make-array mmax :initial-element nil))
 
 ;; 0 and 1 are not primes
@@ -15,17 +7,15 @@
 (setf (aref nums 1) t)
 
 ;; Computing prime numbers
-(for-step (lambda (i)
-            (if (aref nums i)
-                ()
-                (for-step (lambda (j) (setf (aref nums j) t)) (* i 2) mmax i)
-                ))
-          2 (sqrt mmax) 1)
+(loop for i from 2 to (sqrt mmax) by 1 do
+      (when (not (aref nums i))
+        (loop for j from (* i 2) to (- mmax 1) by i do
+              (setf (aref nums j) t))))
 
 ;; Counting prime numbers
 (defvar total 0)
-(for-step (lambda (i) (if (aref nums i)
-                          ()
-                        (incf total))) 0 (- mmax 1) 1)
+(loop for i from 0 to (- mmax 1) do
+      (when (not (aref nums i))
+          (incf total)))
 
 (format t "~D" total)
